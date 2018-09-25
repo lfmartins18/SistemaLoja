e3acd40e8fa040bf140dc78af76b78560b1c24d9
Luiz <luiz18659@gmail.com>
2018-09-25 14:45:00 -0300

    criando a classe ClienteControllers e um list de clientes html

--- src/main/java/ifal/edu/tarde/br/SistemaLoja/controllers/ClienteControllers.java
+++ src/main/java/ifal/edu/tarde/br/SistemaLoja/controllers/ClienteControllers.java
@@ -1,5 +1,28 @@
 package ifal.edu.tarde.br.SistemaLoja.controllers;
 
-public class ClienteControllers {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifal.edu.tarde.br.SistemaLoja.model.Cliente;
+import ifal.edu.tarde.br.SistemaLoja.repositories.ClienteRepository;
+
+@Controller
+@RequestMapping
+public class ClienteControllers {
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	@GetMapping(value= "/list")
+	public String list(ModelMap model) {
+		List<Cliente> clientes = clienteRepository.findAll();
+		model.addAttribute("clienteList", clientes);
+		
+		return "cliente/list";
+	}
 }
