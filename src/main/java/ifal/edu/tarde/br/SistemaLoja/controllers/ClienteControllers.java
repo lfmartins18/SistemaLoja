0af0d70f7adfbe31a1fc77fafc5cc9f54b680d43
Paulo Izidório <paulosp1995@gmail.com>
2018-10-02 16:25:37 -0300

    testando save no formularaio

--- src/main/java/ifal/edu/tarde/br/SistemaLoja/controllers/ClienteControllers.java
+++ src/main/java/ifal/edu/tarde/br/SistemaLoja/controllers/ClienteControllers.java
@@ -1,12 +1,18 @@
 package ifal.edu.tarde.br.SistemaLoja.controllers;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ifal.edu.tarde.br.SistemaLoja.model.Cliente;
 import ifal.edu.tarde.br.SistemaLoja.repositories.ClienteRepository;
@@ -25,4 +31,24 @@ public class ClienteControllers {
 		
 		return "cliente/list";
 	}
+	@RequestMapping(value = { "/novoForm" }, method = RequestMethod.GET)
+	public String newClient(ModelMap model) {
+		
+		Cliente cliente = new Cliente();
+		model.addAttribute("cliente", cliente);
+		model.addAttribute("edit", false);
+		
+		return "cliente/form";
+	}
+
+    @PostMapping(value = "/save")
+    public String saveClient(@RequestParam Map<String, String> params) {
+    	Cliente cliente =  new Cliente();
+    	
+    	cliente.setNomeCompleto(params.get("nomeCompleto"));
+    	cliente.setEmail(params.get("email"));
+    	
+    	clienteRepository.saveAndFlush(cliente);
+    	return "redirect: cliente/list";
+    }
 }
